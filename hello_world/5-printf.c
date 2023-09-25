222f4d1a2e151b9d951d9dbc0e853ec11be57669
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 12:00:10 -0700

    printf doesnt add new line automatically

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Entry point of a program where the execution of a program starts.
+ * Return: Ends the Xcution of a function, & returns control to the calling function.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
