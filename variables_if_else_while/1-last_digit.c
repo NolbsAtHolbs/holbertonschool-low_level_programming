9390357c1ca680a23bdb10d7a9fb64da9ac0e7d1
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 14:46:46 -0700

    no comma test before &&

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, elldee);
 	}
-	else if (elldee < 6, && elldee != 0)
+	else if (elldee < 6 && elldee != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", elldee);
 	}
