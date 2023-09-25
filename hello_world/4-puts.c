f54ff7dafaff0cc2dd7fe583dd4ba2527c988a44
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 10:01:57 -0700

    message starts with " so i needed an escape character \". betty makes us define some functions

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/*
+ 
+ main - the entry point of a program where the execution of a program starts.
+
+
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
