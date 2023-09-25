541b5fd40635ed5cb94ef25cd01f7fe39c4f4c6e
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 16:08:01 -0700

    alphabet exclusions using operand !=

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print every letter of de alphabet lowercase, except q and e
+ * Return: success
+ */
+int main(void)
+{
+	char alphabet, e, q;
+	e = 'e';
+	q = 'q';
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	if (alphabet != e && alphabet != q)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
