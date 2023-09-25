a87238f6f3a8c7df886c33db0e5b7fbeb1a970dd
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 13:42:21 -0700

    random number and sign generator

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints positive, negative, or neutral
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int z;
+	srand(time(0));
+	z = rand() - RAND_MAX / 2;
+	if (z > 0)
+	{
+		printf("%d is positive\n", z);
+	}
+	else if (z < 0)
+	{
+		printf("%d is negative\n", z);
+	}
+	else
+	{
+		printf("%d is zero\n", z);
+	}
+	return (0);
+}
