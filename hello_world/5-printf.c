0127322b9898300054aa8c0394da2ea4cd043280
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 12:10:38 -0700

    man

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
  * main - Entry point of a program where the execution of a program starts.
- * Return: 0 Nds de Xcution of a functn, & retrns cntrl to de calling functn.
+ *
+ * Return: Always 0 Nds de Xcution of a functn, & rtrns cntrl to de cllng fnctn.
  */
 int main(void)
 {
