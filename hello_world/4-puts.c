1f6e5e82a059ec91d58dda89eee46c110144fe02
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 10:09:19 -0700

    ok betty is going to be a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/*
- 
- main - the entry point of a program where the execution of a program starts.
-
-
+/**
+ * main - the entry point of a program where the execution of a program starts.
+ *
+ * return - indicates that the program implementation is accomplished and that the processor can now be uploaded.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
