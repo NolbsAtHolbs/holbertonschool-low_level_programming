eea9600d9cebc2ed519fb64066818c702b9745ab
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 12:07:09 -0700

    spacing must be aut

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
  * main - Entry point of a program where the execution of a program starts.
- * Return: Ends the Xcution of a function, & returns control to the calling function.
+ * Return: 0 Nds de Xcution of a functn, & retrns cntrl to de calling functn.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
