c419508fd678057889914e50d5a51701e017f72c
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 16:40:32 -0700

    easy mick cheasee

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet backwards lowercase
+ * Return: success!
+ */
+int main(void)
+{
+	char tebahpla;
+
+	for (tehbahpla = 'z'; tebahpla >= 'a'; tebahpla--)
+		putchar(tebahpla);
+
+	putchar('\n');
+
+	return (0);
+}
