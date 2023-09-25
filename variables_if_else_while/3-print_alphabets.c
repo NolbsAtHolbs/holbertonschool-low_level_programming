c1f8b349028ac827651f7caf0dca18d3cb6732e7
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 15:49:17 -0700

    doubleover

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print both cases of alphabets
+ * Return: success
+ */
+int main(void)
+{
+	char alphabet, ALPHABET;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+
+	for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
+		putchar(ALPHABET);
+
+	putchar('\n');
+
+	return (0);
