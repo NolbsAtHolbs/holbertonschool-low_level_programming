a482d17524899df794afecf0094e6b7094b3dc66
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 20:08:57 -0700

    broseppi

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,12 @@
-test
+#include "main.h"
+/**
+ *  prints alphabet in lowercase nested style
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		_putchar(letters);
+	_putchar('\n');
+}
