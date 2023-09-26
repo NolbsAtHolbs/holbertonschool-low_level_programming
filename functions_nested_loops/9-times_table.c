8bce2b2090cdd3340e9691b4adc3e20d611cdfb0
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 21:22:20 -0700

    typos

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,12 +17,12 @@ void times_table(void)
 
 			product = x * y;
 
-			if (prod <= 9)
+			if (product <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((product / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
