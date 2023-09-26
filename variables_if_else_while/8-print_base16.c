697fb0cc2a8009f8f310a98cdadc6a81ae167abd
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 17:00:48 -0700

    unoperated numcheck

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16 in lowercase
+ * Return: success!
+ */
+int main(void)
+{
+	int hexnumbers;
+	char hexletters;
+
+	for (hexnumbers = 0; hexnumbers < 10; hexnumbers++)
+		putchar(hexnumbers);
+
+	for (hexletters = 'a'; hexletters < 'f'; hexletters++)
+		putchar(hexletters);
+
+	putchar('\n');
+
+	return (0);
+}
