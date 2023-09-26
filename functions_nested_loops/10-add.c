4b12218f3a0983094e5647a3e1de0bc488b388e7
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 21:32:20 -0700

    added additioning

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds 2 integers
+ * @numberone: first addee
+ * @numbertwo: second addee
+ * Return: result of the addition
+ */
+int add(int numberone, int numbertwo)
+{
+	return (numberone + numbertwo);
+}
