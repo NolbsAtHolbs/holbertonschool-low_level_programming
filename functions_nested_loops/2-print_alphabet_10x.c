d35f84c7860795caf0e777e6ec4c9afa2ff2b32f
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 19:49:40 -0700

    whattest

---
+++ functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  print_alphabet_x10 - prints alphabet 10 times lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int prints = 0;
+	char letters;
+
+	while (prints++ <= 9)
+		
+		for (letters = 'a'; letters <= 'z'; letters++)
+			_putchar(letters);
+	_putchar('\n');
+}
+		
