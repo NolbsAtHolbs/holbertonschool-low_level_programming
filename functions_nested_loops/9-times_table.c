4620d2f81d385cbe12deda6bb349bebd7724748c
NolbsAtHolbs <hikikomode@gmail.com>
2023-09-25 21:21:06 -0700

    rearrangement. lots of rules

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 mutliplication table
+ */
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+
+		for (y = 1; y <= 9; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = x * y;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
