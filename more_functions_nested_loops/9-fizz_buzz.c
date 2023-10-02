7b471d5f0a0ab0f446ff1eeeac35f384f897e423
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 16:39:11 -0700

    percentd needs to know that the int its expecting is n

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
-		else 
+		else
 			if (n % 3 == 0)
 				printf("Fizz ");
 
-		else 
+		else
 			if (n % 5 == 0)
 				printf("Buzz ");
 
