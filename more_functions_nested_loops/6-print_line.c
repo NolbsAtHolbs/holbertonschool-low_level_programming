5834ea5c240b66a7e273581fab431bd76637c38c
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 15:33:14 -0700

    underscore producer

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints _ a lot
+ * @n: integer for params
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
