a09bd0dc56a65c14dd4efe99e7cf996b6901fe35
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 14:31:53 -0700

    tough nesting

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - print 10 copies of the numbers 0-14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 13)
+			{
+				_putchar('1');
+				_putchar(y % 10 + '0');
+			}
+		}
+		_putchar('\n')
+	}
+}
