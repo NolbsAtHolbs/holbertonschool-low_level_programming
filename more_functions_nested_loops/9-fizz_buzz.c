4030de48c2e6b257f030c8b75291c8d131b0d800
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 16:34:14 -0700

    fizzbuzz is so valid

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints numbs 1 - 100 x3s print fizz, x5s print buzz, both = fizzbuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else 
+			if (n % 3 == 0)
+				printf("Fizz ");
+
+		else 
+			if (n % 5 == 0)
+				printf("Buzz ");
+
+		else
+			printf("%d ");
+	n++;
+	}
+return (0);
+}
