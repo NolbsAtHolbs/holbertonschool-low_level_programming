b5d7f8bf06c105eb99010b4af2969a573492f89e
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 15:25:24 -0700

    more math test 16

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,11 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y <= 9)
+			if (y >= 10)
 			{
 				_putchar('1');
-				_putchar(y % 10 + '0');
 			}
-			if (y > 9)
-			{
-				_putchar(y % 10 + '0');
-			}
+			_putchar (y % 10 + '0');
 
 		}
 		_putchar('\n');
