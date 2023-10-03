0f9003d91b5578dc6c06c8faac80af6dcc1ed626
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 18:20:04 -0700

    first pointer file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - point and updates value of n to 98
+ * @n: pointer to the variable to update
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
