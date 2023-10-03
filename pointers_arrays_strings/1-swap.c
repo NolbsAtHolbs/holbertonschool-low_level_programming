6b6faf36259e4a4d572c0aaf86825005436cc56c
NolbsAtHolbs <hikikomode@gmail.com>
2023-10-02 18:43:03 -0700

    pointer pointer

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,22 +1,16 @@
-#include<stdio.h>
-#include<conio.h>
-int swap(int x, int y);
-void main()
+#include "main.h"
+
+/**
+ * swap_int - swaps int values
+ * @a: first pointer
+ * @b: second pointer
+ * Return: self
+ */
+void swap_int(int *a, int *b)
 {
-    int a, b;
-    printf("Enter the value of a= ");
-    scanf("%d", &a);
-    printf("Enter the value of b= ");
-    scanf("%d", &b);
-    swap(a, b);
-    getch();
-}
-int swap(int x, int y)
-{
-    int t;
-    t=x;
-    x=y;
-    y=t;
-    printf("\n\nThe value of a is %d.\n", x);
-    printf("The value of b is %d.\n", y);
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
