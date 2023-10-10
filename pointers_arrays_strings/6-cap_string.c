f7d997cb77d70b5a6dd86540a0785b53474fd9f0
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-09 23:09:26 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ int is_separator(char c)
 	int i = 0;
 	char separators[] = " \t\n,;.!?\"(){}";
 
-    for (separators[i] != '\0'; i++)
+    for (int i = 0; separators[i] != '\0'; i++)
 		{
 			if (c == separators[i])
 			{
