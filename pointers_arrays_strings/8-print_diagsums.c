41f7dd58beb4b1766e38ecef5070b203d0b43eb0
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-09 22:44:52 -0500

    Update 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -19,6 +19,6 @@ void print_diagsums(int *a, int size)
 		secondary_diag_sum += a[i * size + (size - 1 - i)];
 		i++;
 	}
-	printf("Sum of the primary diagonal: %d\n", primary_diag_sum);
-	printf("Sum of the secondary diagonal: %d\n", secondary_diag_sum);
+	printf("%d\n", primary_diag_sum);
+	printf("%d\n", secondary_diag_sum);
 }
