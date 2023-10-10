fb9f5052278c01d42bfc5f8c185157fb7d519a8e
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-09 22:09:30 -0500

    Create 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main3.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to a string
+ * @needle: pointer to a substring in string
+ * Return: substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
