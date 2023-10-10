fdd1b93618b3ceebd262cd75625551bebe2d48a8
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-09 19:27:36 -0500

    Update 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,9 +3,9 @@
 
 /**
  * _strncat - concats strings from one string to another
- * @dest - pointer to the destination string
- * @src - pointer to the source string
- * @n - max num of characters to append from src to dest
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: max num of characters to append from src to dest
  * Return: result
  */
 
