376518d27057cdaecb29c6a5ab75bb8c61d7c472
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-15 20:27:56 -0500

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _puts_recursion - prints a string and new line
+ * @s: the string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
