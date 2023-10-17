899fa64e7e753f7be055ee3e720b9a5905d77baa
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 23:31:49 -0500

    Create 0-isupper.c

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - checks case, positive = upper
+ * @c: character checkee
+ *
+ * Return: 1 if c is uppercase, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
