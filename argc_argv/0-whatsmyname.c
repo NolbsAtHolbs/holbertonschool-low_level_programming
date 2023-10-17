bc99b28efeef1262d038b87680bf83cef2ec89db
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 20:40:43 -0500

    Update 0-whatsmyname.c
    replaced wrong idea

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "libmy.a"
+#include "main.h"
+/**
+  *
+  */
 int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+	else
+	{
+		fprintf(stderr, "Unable to retrieve program name.\n");
+		return (1);
+	}
+}
