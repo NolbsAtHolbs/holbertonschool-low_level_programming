ea4123f36e7f63b2d86f4bf07195649d020d2194
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 19:56:45 -0500

    Update 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,16 +9,14 @@
 int checker(int n, int base)
 {
 	if (base % n == 0 || base < 2)
-  {
+	{
 		return (0);
-  }
-	else 
-  if (n == base - 1)
+	}
+	else if (n == base - 1)
   {
 		return (1);
   }
-	else
-  if (base > n)
+	else if (base > n)
   {
 		return (checker(n + 1, base));
   }
