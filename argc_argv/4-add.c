fc63d29f526f3d5caf871aea3933b107b2f1b2f3
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 22:05:37 -0500

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,46 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
- * is_positive_number - Checks if a string represents a positive number
- * @str: The string to be checked
- * Return: 1 if the string is a positive number, 0 otherwise
- */
-int is_positive_number(const char *str)
-{
-	int i = 0;
-	for (i = 0, str[i] != '\0', i++)
-	{
-		if (!isdigit(str[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-/**
- * main - Adds positive numbers and prints the result
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the arguments
- * Return: 0 if successful, 1 if an error occurs
+ * main - Prints the addition of positive numbers, followed by a new line
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ * Return: If 1 of the nums contains symbols that aren't digits, print 1. Otherwise 0
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	if (argc < 2)
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
 	{
-		printf("0\n");
-		return (0);
-	}
-	int i = 1
-	for (i = 1, i < argc, i++)
-	{
-		if (!is_positive_number(argv[i]))
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+		sum += atoi(argv[num]);
 	}
 	printf("%d\n", sum);
 	return (0);
