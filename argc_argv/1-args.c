5bb4b7697547f667210bea0eded103471fa0f227
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 21:21:40 -0500

    Update 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - Prints number of arguments passed into the program
+ * @argc: Number of arguments
+ * @argv: An array of strings containing the arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+    (void)argv;
+
+	if (argc > 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		printf("0\n");
+	}
 	return (0);
 }
