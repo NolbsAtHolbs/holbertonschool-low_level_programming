f4d658e4b714976a645cc151be0a3f4ecd826c80
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-16 19:17:30 -0500

    Update 5-sqrt_recursion.c
    no mixed declarations?

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _sqrt_recursion - Calculates the natural square root of a number using recursion
  * @n: Square rootee
  * Return: Natural square root of n, or -1 if n does not have a natural square root
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -14,17 +12,17 @@ int _sqrt_recursion(int n)
 		return (-1);
 	}
 	if (n == 0 || n == 1)
+	{
 		return (n);
-
+	}
 	int x = 1;
-	
 	while (x * x <= n)
 	{
 		if (x * x == n)
+		{
 			return (x);
-
+		}
 		x++;
 	}
-
 	return (-1);
 }
