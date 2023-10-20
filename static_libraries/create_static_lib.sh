624c9745c4681aecd0d87235839375452a1838bd
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-19 21:03:22 -0500

    Update create_static_lib.sh

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-for file in *.c
-do
-	gcc -c "$file"
-done
-
-ar -rc liball.a *.o
+gcc -c *.c
+ar rcs liball.a *.o
+rm -f *.o
+echo "Static library 'liball.a' created from all .c files in the current directory."
