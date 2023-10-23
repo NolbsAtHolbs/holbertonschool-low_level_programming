eeea132a1250150dda17cb8e31f7e065955f8e21
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-22 22:46:02 -0500

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,16 +12,16 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *array;
-	
+
 	if (size == 0)
 	{
-		return NULL;
+	return NULL;
 	}
 	array = (char *)malloc(size * sizeof(char));
-	
+
 	if (array == NULL)
 	{
-		return NULL;
+	return NULL;
 	}
 	for (i = 0; i < size; i++)
 	{
