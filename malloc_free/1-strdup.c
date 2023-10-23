375adb535b70ee0c8c7b4f61c136adfd257ce6ea
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-22 23:40:31 -0500

    Update 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _strdup - rtrns pntr 2 newly allctd spce n mmry cntng dplct of th strng gvn as a prmtr
+ * _strdup - rtrns pntr 2 nwly alctd spc n mmry cntng cpy f d strng gvn as prmtr
  * @str: input string to be duplicated
  * Return: pointer to the duplicated string, or NULL on failure
  */
-
 char *_strdup(char *str)
 {
 	char *duplicate;
 	int length = 0;
 	int i = 0;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
