6b5ae681d28cc8afaea8fa6032ec4d2c96b2f961
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-22 22:38:37 -0500

    Update 0-create_array.c
    re'array'nge
    
    no for loop declr

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,19 +9,22 @@
 
 char *create_array(unsigned int size, char c)
 {
-    if (size == 0)
-		{
-			return (NULL);
-		}
-    char *array = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *array = (char *)malloc(size * sizeof(char));
 
-    if (array == NULL)
-		{
-			return (NULL);
-		}
-    for (unsigned int i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
-    return (array);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	unsigned int i = 0;
+	
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
 }
