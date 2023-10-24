228008ca22e168a2ee25e366ed7cd2c3ae8c73b3
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-23 22:41:38 -0500

    Update 3-function_like_macro.h

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,20 +1,6 @@
-#include <stdio.h>
+#ifndef ABSMAF_H
+#define ABSMAF_H
 
-/**
- * main - function-like macro ABS(x)
- * @x: the number for which the absolute value is calculated
- * Return: absolute value of x
- */
+#define ABS(x) ((x < 0) ? (-x) : (x))
 
-int ABS(int x)
-{
-	return ((x < 0) ? -x : x);
-}
-
-int main(void)
-{
-	int num = -5;
-	int result = ABS(num);
-	printf("The absolute value of %d is %d\n", num, result);
-	return (0);
-}
+#endif
