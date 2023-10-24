374744d263a72869383212a41b4547f4277b52d4
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-23 22:14:12 -0500

    Update 3-function_like_macro.h
    betty definitions after #inclusions and before #definitions

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * ABS - computes the absolute value of a number
+ * @x: the number for which the absolute value is calculated
+ * Return: absolute value of x
+ */
+
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
 int main(void)
