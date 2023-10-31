502c61bf4c583d695d5f7f3673202e4d7a66d789
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 23:33:29 -0500

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else
+	{
+		printf("Error: division by zero is not allowed.\n");
+		return (0);
+	}
+}
+
+int op_mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		printf("Error: division by zero is not allowed.\n");
+		return (0);
+	}
+}
