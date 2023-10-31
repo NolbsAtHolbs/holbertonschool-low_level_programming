fa42ea37f76d1439fbfe5595d37e725b4645170a
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 23:15:01 -0500

    Update 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,7 +2,11 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
