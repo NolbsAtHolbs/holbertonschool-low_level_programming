90d424d7c7680c63e65726a018e3083da89ef374
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 23:53:46 -0500

    Update 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,29 +1,33 @@
+#include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct op
-{
-	char *op;
-int (*f)(int a, int b);
-}
-op_t;
-
+/**
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
+
 	int i = 0;
 
-    while (ops[i].op != NULL)
+	while (i < 5)
 	{
-		if (*ops[i].op == *s)
-			return ops[i].f;
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
 		i++;
 	}
-	return (NULL);
+
+	return (0);
 }
