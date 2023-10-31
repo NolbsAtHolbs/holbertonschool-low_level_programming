acc40769f4964a61aecbc872cabfa9f568f89a8a
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 23:57:59 -0500

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,42 +1,70 @@
+#include "3-calc.h"
+#include <stdlib.h>
 #include <stdio.h>
-
+/**
+  * op_add - adds
+  * @a: var
+  * @b: var2
+  *
+  * Return: sum
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+  * op_sub - subtracts
+  * @a: var
+  * @b: var2
+  *
+  * Return: result
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+  * op_mul - multiplies
+  * @a: var
+  * @b: var2
+  *
+  * Return: product
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+  * op_div - divides
+  * @a: var
+  * @b: var2
+  *
+  * Return: result
+  */
 int op_div(int a, int b)
 {
-	if (b != 0)
+	if (b == 0)
 	{
-		return (a / b);
+		printf("Error\n");
+		exit(100);
 	}
-	else
-	{
-		printf("Error: division by zero is not allowed.\n");
-		return (0);
-	}
-}
 
+	return (a / b);
+}
+/**
+  * op_mod - modulo
+  * @a: var
+  * @b: var2
+  *
+  * Return: result
+  */
 int op_mod(int a, int b)
 {
-	if (b != 0)
+	if (b == 0)
 	{
-		return (a % b);
+		printf("Error\n");
+		exit(100);
 	}
-	else
-	{
-		printf("Error: division by zero is not allowed.\n");
-		return (0);
-	}
+
+	return (a % b);
 }
