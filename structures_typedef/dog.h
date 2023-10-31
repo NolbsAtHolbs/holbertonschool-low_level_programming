8930d7be736bf6934cc14664619ea2daae89ad58
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 19:43:46 -0500

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+#endif
