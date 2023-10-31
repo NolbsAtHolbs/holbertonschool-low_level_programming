1e2ad49c082eb234858aa7b093038d90ee2d028f
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 21:18:03 -0500

    Update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -9,5 +9,6 @@ typedef void (*print_name_function)(char *);
 
 void print_name(char *name, print_name_function f);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
