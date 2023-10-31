127f601a7b3505730b5ac45ce918d168ca0051c0
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 20:37:32 -0500

    Update 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,5 +1,17 @@
-test
-	tab test
- space test
+#include <stdlib.h>
+#include "dog.h"
 
-enter test
+/**
+ * free_dog - Frees a dog structure and its fields
+ * @d: Pointer to the dog structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
