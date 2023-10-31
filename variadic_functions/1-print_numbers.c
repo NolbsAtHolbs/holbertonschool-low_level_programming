44bccabde9290d246b8ae75c7168aa5bbdf83f38
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-10-30 23:06:31 -0500

    Update 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,13 +4,15 @@
 #include <stdlib.h>
 /**
  * print_numbers - prints numbers, followed by a new line
- * 
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
