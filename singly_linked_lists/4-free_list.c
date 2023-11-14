50e8fa47b6d5ff448c1574556bb2eda4a47b477e
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:01:11 -0600

    Create 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+  * free_list - frees a list_t list
+  * @head: top of list
+  */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+  {
+    return;
+  }
+  free_list(head->next);
+	free(head->str);
+	free(head);
+}
