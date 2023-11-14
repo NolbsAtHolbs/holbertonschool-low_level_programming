8dbae940017752cc2bb2e000f2fb56d3b088c899
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 23:16:02 -0600

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-  * read_textfile - reads a text file and prints it to the POSIX standard output
+  * read_textfile - reads a text file & prints it to the POSIX standard output
   * @filename: name of file to be read
   * @letters: number of characters to be printed
   * Return: the actual number of letters it could read and print, 0 if fail
