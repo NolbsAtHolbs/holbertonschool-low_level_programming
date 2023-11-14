b5ce86d3bc926cb926ab9ef0b3dcfaa87e8d6444
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 23:25:32 -0600

    Create 2-append_text_to_file.c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  * append_text_to_file - appends text at the end of a file
+  * @filename: name of the file, return -1 if NULL
+  * @text_content: NULL termd strng to add at end of the file, do not append if NULL 
+  * Return: 1 on success, -1 on failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int append_file, len, wr_stat;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	append_file = open(filename, O_WRONLY | O_APPEND);
+	
+	if (append_file == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	for (len = 0; text_content[len]; len++)
+	{
+		;
+	}
+	wr_stat = write(append_file, text_content, len);
+	close(append_file);
+	return (wr_stat == -1 ? -1 : 1);
+}
