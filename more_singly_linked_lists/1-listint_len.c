bfab1c9f9c1ee019604767c01d3a40a3de9e68c3
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:18:31 -0600

    Create 1-listint_len.c

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: node in linked list
+ * Return: number of elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *c;
+	size_t count;
+
+	for (c = h, count = 0; c != NULL; count++, c = c->next)
+    {
+    ;
+    }
+	return (count);
+}
