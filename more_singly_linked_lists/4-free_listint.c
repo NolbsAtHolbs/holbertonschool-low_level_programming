adf87ef38e6d8d1d499ea3ac9436e6919186d48d
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:35:52 -0600

    Create 4-free_listint.c

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * free_listint - frees a listint_t list
+  * @head: first node in list
+  */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+    free_listint(head->next);
+	free(head);
+}
