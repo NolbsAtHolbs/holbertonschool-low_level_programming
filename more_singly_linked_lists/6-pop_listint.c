dca5776a42f18c3668e3afe39f145e453d7ad4bd
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:44:13 -0600

    Create 6-pop_listint.c

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t linked list, and returns the head node’s data (n)
+ * @head: first node in linked list
+ * Return: 0 if linked list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *hold;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	hold = *head;
+	num = hold->n;
+	*head = hold->next;
+	free(hold);
+
+	return (num);
+}
