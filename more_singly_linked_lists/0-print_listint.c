4c0b2dc00469db7fd7e06cae651123fcfaab8ce8
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:12:38 -0600

    Update 0-print_listint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,18 @@
-sample text
+#include <stdio.h>
+#include "lists.h"
+/**
+  * print_listint - prints all elements of a list_t list
+  * @h: singly linked list to print
+  * Return: number of nodes in list
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
