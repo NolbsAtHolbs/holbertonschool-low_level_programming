2dc7e3376c6ca7d4c9715afb30a1fe9dfb1a0861
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 21:26:44 -0600

    Update 2-add_nodeint.c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,10 +14,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-  {
+	{
 		return (NULL);
-  }
-  new->n = n;
+	}
+	new->n = n;
 	new->next = *head;
 	*head = new;
 	return (*head);
