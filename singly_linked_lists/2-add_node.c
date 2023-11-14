fbc4d19b4f41bee86ded0fbdfd8d59c9fc1bd01e
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-13 20:49:04 -0600

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,33 +8,53 @@
  * @str: string
  * @next: points to next node
  */
-typedef struct list_s
+int _strlen(const char *s)
 {
-char *str;
-struct list_s *next;
-} list_t;
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: double pointer to the head of the list
- * @str: string used in the new node
- * Return: address of the new element, or NULL if it failed
- */
+  * add_node - adds a new node at the beginning of a list_t list
+  * @head: current place in list
+  * @str: string to add to head
+  * Return: pointer to current position in list
+  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	int i, len;
+	char *content;
+	list_t *new;
+
+	if (str == NULL || head == NULL)
 	{
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+
+	if (content == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	for (i = 0; str[i]; i++)
+		{
+		content[i] = str[i];
+		}
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
