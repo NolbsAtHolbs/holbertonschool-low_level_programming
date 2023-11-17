216909381b2a5c6aed9c09e6a6912f22a027ac08
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-16 19:53:16 -0600

    created 2 files - test on new platform

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_dlistint - prints all the elements
+ *
+ *
+ */
+ size_t print_dlistint(const dlistint_t *h)
+ {
+    
+ }
