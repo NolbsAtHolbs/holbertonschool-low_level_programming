1c7820419296e99dda9efb6203c453cec07f2be3
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-21 00:41:22 -0600

    new function

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,10 +8,11 @@ void hash_table_delete(hash_table_t *ht)
 {
 	if (ht == NULL)
 		return;
+	
+	long unsigned int i = 0;
 
-    int i = 0;
-
-	while (i < ht->size) {
+	while (i < ht->size)
+	{
         hash_node_t *current = ht->array[i];
         hash_node_t *next;
 
