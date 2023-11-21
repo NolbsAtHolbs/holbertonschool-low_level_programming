81d08d64328981e604eb814356b6c202a6a4ed08
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 22:02:20 -0600

    made function

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - insrts new node @ givn positn in a dlistint_t list
+* @h: double pointer to the head of the list
+* @idx: index at which the new node should be inserted
+* @n: integer data for the new node
+* Return: address of the new node, or NULL if it failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current;
+	unsigned int count = 0;
+
+  new_node = malloc(sizeof(dlistint_t));
+  if (new_node == NULL)
+  {
+      return (NULL);
+  }
+
+  new_node->n = n;
+  new_node->next = NULL;
+  new_node->prev = NULL;
+
+  if (idx == 0)
+  {
+      new_node->next = *h;
+      if (*h != NULL)
+      {
+          (*h)->prev = new_node;
+      }
+      *h = new_node;
+  }
+  else
+  {
+      current = *h;
+      while (current != NULL && count < idx)
+      {
+          current = current->next;
+          count++;
+      }
+
+      if (current == NULL)
+      {
+          free(new_node);
+          return (NULL);
+      }
+
+      new_node->next = current;
+      new_node->prev = current->prev;
+      current->prev->next = new_node;
+      current->prev = new_node;
+  }
+
+  return (new_node);
+}
---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+* delete_dnodeint_at_index - deletes node at given index in a dlistint_t list
+* @head: double pointer to the head of the list
+* @index: index of the node to delete
+* Return: 1 if it succeeded, -1 if it failed
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *temp;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+
+		free(temp);
+		return (1);
+	}
+	current = *head;
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+		return (-1);
+
+	temp = current;
+	current->prev->next = current->next;
+	if (current->next != NULL)
+       current->next->prev = current->prev;
+
+   free(temp);
+   return (1);
+}
