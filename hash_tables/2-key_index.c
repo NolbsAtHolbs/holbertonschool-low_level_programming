5ad3cc08138b13412e0d976c46ba4ba8083364d0
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 23:51:18 -0600

    made math function

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,1 +1,11 @@
-placeholder
+#include "hash_tables.h"
+/**
+ * key_index - gives you the index of a key
+ * @key: unique identifier associated with each data element
+ * @size: size of the array of the hash table
+ * Return: index the key/value pair is stored at in array of hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
