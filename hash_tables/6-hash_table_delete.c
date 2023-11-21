89a2b8e0b0c29fed0a943ce2aa55f43356b8651e
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-21 00:32:33 -0600

    delete hash table function...?

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,1 +1,24 @@
-placeholder
+#include "hash_tables.h"
+/**
+* hash_table_delete - deletes a hash table
+* @ht: hash table to delete
+* Return: void
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return();
+
+	for (int i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
+		hash_node_t *next;
+
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
+	}
+}
