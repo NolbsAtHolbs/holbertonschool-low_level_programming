40b1dae0fd7b868c89ee90fef4b1fa39d57ad64b
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-21 00:12:18 -0600

    function

--- hash_tables/5-hash-table_print.c
+++ hash_tables/5-hash-table_print.c
@@ -1,1 +1,35 @@
-placeholder
+#include "hash_tables.h"
+/**
+* hash_table_print - prints a hash table
+* @ht: hash table
+* Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current, *next;
+	unsigned long int count;
+	int pair = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (count = 0; count < ht->size; count++)
+	{
+		current = ht->array[count];
+
+		while (current != NULL)
+		{
+			next = current->next;
+
+			if (pair)
+				printf(", ");
+
+			printf("'%s': '%s'", current->key, current->value);
+			pair = 1;
+			current = next;
+		}
+	}
+	printf("}\n");
+}
