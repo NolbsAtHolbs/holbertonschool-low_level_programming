51915426f150e9158a6dc24d8fdd5c2a27b598ba
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 18:27:10 -0600

    format testing

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,20 +3,20 @@
 #include <string.h>
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a doubly linked list
- * @h: head of linked list
- * Return: number of nodes
- */
- size_t print_dlistint(const dlistint_t *h)
- {
-    const dlistint_t *currentNode = h;
-    size_t nodeCount = 0;
+print_dlistint - prints all the elements of a doubly linked list
+@h: head of linked list
+Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *currentNode = h;
+	size_t nodeCount = 0;
 
-    while (currentNode != NULL)
+	while (currentNode != NULL)
     {
-        printf("%d\n", currentNode->n);
-        currentNode = currentNode->next;
-        nodeCount++;
-    }
-    return (nodeCount);
- }
+		printf("%d\n", currentNode->n);
+		currentNode = currentNode->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
