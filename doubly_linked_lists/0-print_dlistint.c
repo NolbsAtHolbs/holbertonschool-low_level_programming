c600c1b6d0a2b7edc80713d6314f9a772ffe492d
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 18:03:08 -0600

    lists file contains typedef struct for dlistint_t, and all prototypes

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 /**
  * print_dlistint - prints all the elements
- *
- *
+ * @h: head of linked list
+ * return: number of nodes
  */
  size_t print_dlistint(const dlistint_t *h)
  {
-    
+    const dlistint_t *currentNode = h;
+    size_t nodeCount = 0;
+
+    while (currentNode != NULL)
+    {
+        printf("%d\n", current->n);
+        currentNode = current->next;
+        nodeCount++;
+    }
+    return (nodeCount)
  }
