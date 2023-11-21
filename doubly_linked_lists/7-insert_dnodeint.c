e9449ff27eeb51bdcacff7169295bf7f2e639dde
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 22:11:40 -0600

    optimization

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
@@ -13,45 +12,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *current;
 	unsigned int count = 0;
 
-  new_node = malloc(sizeof(dlistint_t));
-  if (new_node == NULL)
-  {
-      return (NULL);
-  }
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-  new_node->n = n;
-  new_node->next = NULL;
-  new_node->prev = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
-  if (idx == 0)
-  {
-      new_node->next = *h;
-      if (*h != NULL)
-      {
-          (*h)->prev = new_node;
-      }
-      *h = new_node;
-  }
-  else
-  {
-      current = *h;
-      while (current != NULL && count < idx)
-      {
-          current = current->next;
-          count++;
-      }
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 
-      if (current == NULL)
-      {
-          free(new_node);
-          return (NULL);
-      }
-
-      new_node->next = current;
-      new_node->prev = current->prev;
-      current->prev->next = new_node;
-      current->prev = new_node;
-  }
-
-  return (new_node);
+	*h = new_node;
+	}
+	else
+	{
+		current = *h;
+		while (current != NULL && count < idx)
+		{
+			current = current->next;
+			count++;
+		}
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->next = current;
+		new_node->prev = current->prev;
+		current->prev->next = new_node;
+		current->prev = new_node;
+	}
+	return (new_node);
 }
