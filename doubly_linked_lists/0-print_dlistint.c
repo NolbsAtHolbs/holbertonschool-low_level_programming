953da763d0850d91d458098f6f298f511fd2219b
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 18:28:05 -0600

    format fixing

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include "lists.h"
 /**
-print_dlistint - prints all the elements of a doubly linked list
-@h: head of linked list
-Return: number of nodes
+* print_dlistint - prints all the elements of a doubly linked list
+* @h: head of linked list
+* Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
