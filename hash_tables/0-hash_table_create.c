dda2eb55a203144ceadfb68b70ca39435afb843a
NolbsAtHolbs <hikikomode@gmail.com>
2023-11-20 23:47:11 -0600

    new function

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,24 +1,29 @@
 #include "hash_tables.h"
 /**
 * hash_table_create - creates a hash table
-* @size: size of array
-* Return: pointer to newly created hash table, or NULL if something goes wrong
+* @size: size of the array
+* Return: pointer to the new hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table *new_table = malloc(sizeof(hash_table));
+	hash_table_t *new_table;
+	unsigned long int temp = 0;
+
+	new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->table = malloc(sizeof(hash_node *) * size);
-	if (new_table->table == NULL)
-	{
-		free(new_table);
-		return NULL;
-	}
-	for (unsigned long int i = 0; i < size; i++)
-		new_table->table[i] = NULL;
+	new_table->array = malloc(sizeof(hash_node_t *) * new_table->size);
 
-	return new_table;
+	if (new_table->array == NULL)
+		return (NULL);
+
+	while (temp < new_table->size)
+	{
+		temp++;
+		new_table->array[temp] = NULL;
+	}
+	return (new_table);
 }
