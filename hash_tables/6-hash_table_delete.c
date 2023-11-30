47101b3c5a21c9a23e61983b1459a714610ecce7
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-30 01:02:24 -0600

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-    long unsigned int i = 0;
+    unsigned long int i = 0;
     hash_node_t *use = NULL;
 
     while (i < ht->size)
