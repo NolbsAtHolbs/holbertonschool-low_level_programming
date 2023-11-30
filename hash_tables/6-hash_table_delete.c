2383fb7000fb4e439be2566ddf69557ddb207165
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-11-30 01:06:10 -0600

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -14,23 +14,22 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-    while (i < ht->size)
-    {
-        if (ht->array[i] != NULL)
-        {
-            use = ht->array[i];
-            while (ht->array[i] != NULL)
-            {
-                use = use->next;
-                free(ht->array[i]->key);
-                free(ht->array[i]->value);
-                free(ht->array[i]);
-                ht->array[i] = use;
-            }
-        }
-        i++;
-    }
-
-    free(ht->array);
-    free(ht);
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			use = ht->array[i];
+			while (ht->array[i] != NULL)
+			{
+				use = use->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = use;
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
