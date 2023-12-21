186294c7827ef7e76057e923ba20939501a6363e
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2023-12-21 22:25:00 +0000

    update c file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,1 +1,13 @@
-sample
+#include "search_algos.h"
+/*
+* linear_search - searches for a value in a sorted array of integers using the
+* binary search algorithm
+*
+* @array: sorted array of integers
+* @size: size of array
+* @value: value being searched for
+*/
+int linear_search(int *array, size_t size, int value);
+{
+    
+}
