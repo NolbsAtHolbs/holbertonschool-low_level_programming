e0c33f9f2440d0e3427daca6713922c4a6d56f71
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 20:27:52 -0600

    binary search algo function

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,5 +10,37 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid = (low + high) / 2;
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+
+		for (i = low; i <= high; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		if (array[mid] > value)
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
 }
