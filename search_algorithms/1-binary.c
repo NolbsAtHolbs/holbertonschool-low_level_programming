224b11832893c1db1db447cb593e09cb6aba7b27
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 20:43:10 -0600

    check corrections

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -26,6 +26,11 @@ int binary_search(int *array, size_t size, int value)
 		for (i = low; i <= high; i++)
 		{
 			printf("%d ", array[i]);
+
+			if (i < high)
+			{
+				printf(", ");
+			}
 		}
 		printf("\n");
 
