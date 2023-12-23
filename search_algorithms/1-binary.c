053fc50700090d452ca95d71068a87c494452fae
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 20:48:35 -0600

    type corrections

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,10 +10,10 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t mid = (low + high) / 2;
-	size_t i;
+	int low = 0;
+	int high = size - 1;
+	int mid = (low + high) / 2;
+	int i;
 
 	if (array == NULL)
 	{
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 
 		for (i = low; i <= high; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d", array[i]);
 
 			if (i < high)
 			{
