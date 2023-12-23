314f7f0c5bbd1ebfcad1578262efd473a7ca9a6c
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 20:00:48 -0600

    linear search function

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,5 +10,18 @@
 */
 int linear_search(int *array, size_t size, int value);
 {
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d\n", array[i]);
 
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
